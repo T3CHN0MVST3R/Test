f3c723a5b2bed906e07219eafc962decd03bc10a
Denzo <denzomaster@192.168.2.130>
2024-09-23 17:14:14 +0300

    FUCK

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    int t = 12;
+    int t2 = 11;
+
+    public static void main(String[] args) {
+        // Now it works because Test has a default constructor
+        Test test = new Test();
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,22 @@
+public class Test {
+
+    int i;
+    int j;
+
+    // Default constructor
+    public Test() {
+        // Optionally initialize values here
+    }
+
+    public Test(int i) {
+        this.i = i;  // Use 'this' to refer to the instance variable
+    }
+
+    public Test(int i, int j) {
+        this.i = i;  // Use 'this' to refer to the instance variable
+        this.j = j;
+    }
+}
+
+
+
